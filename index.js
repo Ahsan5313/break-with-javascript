b0fa2346b8b73463de460fcfc370a6573c795c85
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-07 12:06:13 +0600

    add html and javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+for(var x=1;x<=100;x=x+1){
+     
+       document.write(" " +x);
+       if(x==10){
+        break
+   }
+}
+
+for(var x=1;x<=100;x++){
+            if(x==10){
+                break
+            }
+            document.write(" "+x);
+}
